bec0640946a847249f6670b32c911008aac6ce8f
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2025-09-22 13:03:55 +0300

    delete ja post

--- bookstore/src/main/java/com/example/bookstore/controller/BookRestController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/BookRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bookstore.model.Book;
@@ -22,15 +23,25 @@ public class BookRestController {
         this.bookRepository = bookRepository;
     }
 
-    @RequestMapping(value="/books", method=RequestMethod.GET)
+    @RequestMapping(value="/api/books", method=RequestMethod.GET)
     public @ResponseBody List<Book> bookListRest() {
         return (List<Book>) bookRepository.findAll();
     }
 
-    @RequestMapping(value="/books/{id}", method=RequestMethod.GET)
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.GET)
     public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
         return bookRepository.findById(bookId);
     }
+
+    @RequestMapping(value="/api/books", method=RequestMethod.POST)
+    public @ResponseBody Book addBook(@RequestBody Book newBook) {
+        return bookRepository.save(newBook);
+}
+
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
+    public void deleteBook(@PathVariable("id") Long bookId) {
+        bookRepository.deleteById(bookId);
+}
 
 
 
